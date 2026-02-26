2e57bc0381ee80892c89f47d9a26b8cbf1a580d8
Domitori <domitori.starlight@gmail.com>
2026-02-26 13:38:35 +0200

    fix2

---
+++ src/pages/Home/Experience/Experience.jsx
@@ -0,0 +1,15 @@
+import styles from "@/pages/Home/Experience/Experience.module.css";
+export default function Experience() {
+  return (
+    <section id="experience" className={styles.experienceSection}>
+      <h2 className={styles.experienceTitle}>Досвід роботи</h2>
+      <p className={styles.experienceText}>
+        Я працював у компанії <span className={styles.highlight}>gtool</span>,
+        де основна діяльність була пов’язана з програмним кодом: додаванням
+        товарів, зміною логіки, переписом стилів та компонентів. Я писав та
+        оптимізував код, вносив нові функції і редагував існуючі, щоб покращити
+        продуктивність і зовнішній вигляд сайту.
+      </p>
+    </section>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import Hero from "@/pages/Home/Hero/Hero";
 import Skills from "@/pages/Home/Skills/Skills";
 import Contact from "@/pages/Home/Contact/Contact";
 import Project from "./Project/Project";
+import Experience from "./Experience/Experience";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Skills />
+      <Experience />
       {/* <Project /> */}
       <Contact />
     </>
